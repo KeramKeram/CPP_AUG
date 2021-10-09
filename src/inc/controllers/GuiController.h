2030ba2a06fc4cbc1f9875c24ce52cdbb14356d1
KeramKeram <rev@autograf.pl>
2021-10-09 19:50:09 +0200

    GuiController: Add empty functions for menu calls.
    Rotation, path for image, ok button.

--- src/inc/controllers/GuiController.h
+++ src/inc/controllers/GuiController.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include "common/CommonEnums.h"
+
+#include <string>
+
 namespace controllers {
     class GuiController {
     public:
+        void setRotation(RotateCode code, int percent);
 
+        void setPathToImages(std::string path);
+
+        void okButton();
     };
 }
--- src/src/controllers/GuiController.cpp
+++ src/src/controllers/GuiController.cpp
@@ -1,1 +1,18 @@
 #include "controllers/GuiController.h"
+
+#include <iostream>
+
+namespace controllers {
+
+    void GuiController::setRotation(RotateCode code, int percent) {
+        std::cout << toUnderlyingType(code) << percent;
+    }
+
+    void GuiController::setPathToImages(std::string path) {
+        std::cout << path;
+    }
+
+    void GuiController::okButton() {
+
+    }
+}
