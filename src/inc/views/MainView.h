f561cd15e22460fe568fd27f32050a1816798637
KeramKeram <rev@autograf.pl>
2021-10-09 20:22:57 +0200

    MainView: Move most of the code for view from main to view.

--- src/inc/views/MainView.h
+++ src/inc/views/MainView.h
@@ -1,1 +1,31 @@
 #pragma once
+
+#include "ftxui/component/captured_mouse.hpp"  // for ftxui
+#include "ftxui/component/component.hpp"  // for Menu, Renderer, Horizontal, Vertical
+#include "ftxui/component/component_base.hpp"  // for ComponentBase
+#include "ftxui/component/screen_interactive.hpp"  // for Component, ScreenInteractive
+#include "ftxui/dom/elements.hpp"  // for text, Element, operator|, window, flex, vbox
+#include "views/IView.h"
+
+#include <array>
+#include <vector>
+#include <string>
+#include <stdlib.h>  // for EXIT_SUCCESS
+#include <memory>    // for allocator, __shared_ptr_access
+
+
+namespace views {
+    class MainView : public IView {
+    public:
+
+        void show();
+
+    private:
+        ftxui::Component Window(std::string title, ftxui::Component component);
+
+        std::array<int, 2> mMenuSelected{0, 0};
+        std::vector<std::vector<std::string>> mMenuEntries;
+        int mMenuSelectedGlobal;
+        ftxui::Component mGlobalMenu;
+    };
+}
--- src/src/views/MainView.cpp
+++ src/src/views/MainView.cpp
@@ -1,2 +1,36 @@
 #include "views/MainView.h"
 
+namespace views {
+    void MainView::show() {
+        auto info = ftxui::Renderer([&] {
+            auto g = static_cast<std::size_t>(mMenuSelectedGlobal);
+            auto menu_selected_g = static_cast<std::size_t>(mMenuSelectedGlobal[g]);
+            std::string value = mMenuEntries[g][menu_selected_g];
+            return window(ftxui::text("Content"),  //
+                          ftxui::vbox({
+                                       ftxui::text("menu_selected_global = " + std::to_string(g)),
+                                       ftxui::text("menu_selected[0]     = " +
+                                            std::to_string(mMenuSelected[0])),
+                                       ftxui::text("menu_selected[1]     = " +
+                                            std::to_string(mMenuSelected[1])),
+                                       ftxui::text("Value                = " + value),
+                               })) |
+                    ftxui::flex;
+        });
+
+        auto global = ftxui::Container::Horizontal({
+                                                    mGlobalMenu,
+                                                    info,
+                                            });
+
+        auto screen = ftxui::ScreenInteractive::TerminalOutput();
+        screen.Loop(global);
+    }
+
+    ftxui::Component MainView::Window(std::string title, ftxui::Component component) {
+        return Renderer(component, [component, title] {  //
+            return window(ftxui::text(title), component->Render()) | ftxui::flex;
+        });
+    }
+}
+
