0be1a3ddc1a070cdfab211f7341cef588fb54c11
KeramKeram <rev@autograf.pl>
2021-10-18 22:41:22 +0200

    Model: Create basic interface for model.

--- src/inc/models/IModel.h
+++ src/inc/models/IModel.h
@@ -1,5 +1,21 @@
 #pragma once
 
-namespace models{
+#include <memory>
 
+namespace models {
+    template<class T>
+    class IModel {
+    public:
+        virtual ~IModel() = default;
+
+        virtual void add(std::shared_ptr<T>) = 0;
+
+        void clear() = 0;
+
+        [[nodiscard]] virtual int size() const = 0;
+
+        virtual bool hasNext() const = 0;
+
+        virtual shared_ptr<T>& next() = 0;
+    };
 }
