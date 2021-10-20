463f7300666c6042fb32a297e5e07a13ccdce598
KeramKeram <rev@autograf.pl>
2021-10-20 20:56:48 +0200

    OperationModel: Basic implementation of model.

--- src/inc/models/OperationModel.h
+++ src/inc/models/OperationModel.h
@@ -1,1 +1,34 @@
 #pragma once
+
+#include "models/IModel.h"
+
+#include <condition_variable>
+#include <mutex>
+#include <vector>
+
+namespace models {
+    template<class T>
+    class MOperationModel : public IModel<T> {
+    public:
+        ~MOperationModel() override = default;
+
+        void add(std::shared_ptr<T> ptr) override {
+            std::lock_guard<std::mutex> lk(mMutex);
+            mModel.template emplace_back(ptr);
+            mCondVar.notify_one();
+        }
+
+        bool hasNext() const override {
+            return false;
+        }
+
+        std::shared_ptr<T> &next() override {
+            return nullptr;
+        }
+
+    private:
+        std::vector<std::shared_ptr<T>> mModel;
+        std::mutex mMutex;
+        std::condition_variable mCondVar;
+    };
+}
