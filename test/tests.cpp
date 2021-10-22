b0534bb0ed6f82289981bc5ebc43f205cb5d0c29
KeramKeram <rev@autograf.pl>
2021-10-22 20:46:10 +0200

    Tests: Introduce tests.
    Add first two tests for operation model.

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,14 +1,18 @@
 #include <catch2/catch.hpp>
+#include "models/OperationModel.h"
 
-unsigned int Factorial(unsigned int number)// NOLINT(misc-no-recursion)
+TEST_CASE("Models with data", "[operationsmodel]")
 {
-  return number <= 1 ? number : Factorial(number - 1) * number;
-}
+    models::OperationModel<int> model;
+    SECTION("Test with one element.") {
+        auto item1 = std::make_shared<int>(1);
+        model.add(item1);
+        REQUIRE(model.hasNext() == true);
+    }
 
-TEST_CASE("Factorials are computed", "[factorial]")
-{
-  REQUIRE(Factorial(1) == 1);
-  REQUIRE(Factorial(2) == 2);
-  REQUIRE(Factorial(3) == 6);
-  REQUIRE(Factorial(10) == 3628800);
+    SECTION("Test with two element.") {
+        auto item2 = std::make_shared<int>(2);
+        model.add(item2);
+        REQUIRE(model.hasNext() == true);
+    }
 }
