b262358dfdafb8dc5999cfc77f536e6343d7f74b
KeramKeram <rev@autograf.pl>
2021-11-06 19:41:18 +0100

    DirectoryFilter: Add UT who checks if filter empty list wil return empty list too.

--- test/DirectoryFilterTests.cpp
+++ test/DirectoryFilterTests.cpp
@@ -3,6 +3,10 @@
 
 TEST_CASE("Filtering empty files list", "[io]")
 {
-
+    io::DirectoryFilter filter;
+    std::vector<std::string> emptyVector;
+    SECTION("Test filtering by extension empty list, return list need to be empty.") {
+        REQUIRE(filter.filterByExtension(emptyVector).size() == 0);
+    }
 }
 
