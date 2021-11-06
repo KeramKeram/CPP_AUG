096b7c3d853c989f6b62ccc496785af28797f8e5
KeramKeram <rev@autograf.pl>
2021-11-06 21:10:52 +0100

    DirectoryFilter: Autoformat from clang.

--- src/inc/io/DirectoryFilter.h
+++ src/inc/io/DirectoryFilter.h
@@ -4,8 +4,9 @@
 #include <vector>
 
 namespace io {
-class DirectoryFilter {
-public:
-    static std::vector<std::string> filterByExtension(const std::vector<std::string> &files, const std::vector<std::string> &extensions);
-};
-}
+    class DirectoryFilter {
+    public:
+        static std::vector<std::string> filterByExtension(const std::vector<std::string> &files,
+                                                          const std::vector<std::string> &extensions);
+    };
+}// namespace io
--- src/src/io/DirectoryFilter.cpp
+++ src/src/io/DirectoryFilter.cpp
@@ -5,10 +5,11 @@
 
 namespace io {
 
-    std::vector<std::string> DirectoryFilter::filterByExtension(const std::vector<std::string> &files, const std::vector<std::string> &extensions) {
+    std::vector<std::string> DirectoryFilter::filterByExtension(const std::vector<std::string> &files,
+                                                                const std::vector<std::string> &extensions) {
         std::vector<std::string> output;
         for (const auto &extension : extensions) {
-            std::copy_if(files.begin(), files.end(),std::back_inserter(output), [extension = extension](auto file) {
+            std::copy_if(files.begin(), files.end(), std::back_inserter(output), [extension = extension](auto file) {
                 using namespace std::filesystem;
                 path filePath(file);
                 if (auto ex = filePath.extension(); ex.empty() || ex.u8string().compare(extension) != 0) {
