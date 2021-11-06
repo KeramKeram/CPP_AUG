53ab4892f1cd85241e776ae6543fc73c09344b3c
KeramKeram <rev@autograf.pl>
2021-11-06 19:51:43 +0100

    DirectoryFilter: Add UT for testing filtering set by existing and no existing extensions.

--- test/DirectoryFilterTests.cpp
+++ test/DirectoryFilterTests.cpp
@@ -10,3 +10,44 @@ TEST_CASE("Filtering empty files list", "[io]")
     }
 }
 
+TEST_CASE("Filtering list by one extension", "[io]")
+{
+    io::DirectoryFilter filter;
+    std::vector<std::string> files = {"main.cpp", "main.h", "foo.c", "foo.hpp", "readme.txt"};
+
+    SECTION("Test filtering by cpp extension") {
+        std::vector<std::string> extensions = {".cpp"};
+        REQUIRE(filter.filterByExtension(files, extensions).size() == 1);
+    }
+
+    SECTION("Test filtering by c extension") {
+        std::vector<std::string> extensions = {".c"};
+        REQUIRE(filter.filterByExtension(files, extensions).size() == 1);
+    }
+
+    SECTION("Test filtering by hpp extension") {
+        std::vector<std::string> extensions = {".hpp"};
+        REQUIRE(filter.filterByExtension(files, extensions).size() == 1);
+    }
+
+    SECTION("Test filtering by txt extension") {
+        std::vector<std::string> extensions = {".txt"};
+        REQUIRE(filter.filterByExtension(files, extensions).size() == 1);
+    }
+
+    SECTION("Test filtering by . extension") {
+        std::vector<std::string> extensions = {"."};
+        REQUIRE(filter.filterByExtension(files, extensions).size() == 0);
+    }
+}
+
+TEST_CASE("Filtering files list by not existing extension in set", "[io]")
+{
+    io::DirectoryFilter filter;
+    std::vector<std::string> files = {"main.cpp", "main.h", "foo.c", "foo.hpp", "readme.txt"};
+
+    SECTION("Test filtering by md extension") {
+        std::vector<std::string> extensions = {".md"};
+        REQUIRE(filter.filterByExtension(files, extensions).size() == 0);
+    }
+}
