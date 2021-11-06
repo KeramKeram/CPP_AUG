db37b910cba2a86504dd9162efb27e82d5a06a80
KeramKeram <rev@autograf.pl>
2021-11-06 19:35:52 +0100

    UT: Add empty tests for DirectoryFilter.

--- src/src/io/DirectoryFilter.cpp
+++ src/src/io/DirectoryFilter.cpp
@@ -3,6 +3,9 @@
 namespace io {
 
     std::vector<std::string> DirectoryFilter::filterByExtension(std::vector<std::string> files) {
+        for(auto elem : files) {
+            files.clear();
+        }
         return std::vector<std::string>();
     }
 }
---
+++ test/DirectoryFilterTests.cpp
@@ -0,0 +1,8 @@
+#include <catch2/catch.hpp>
+#include "io/DirectoryFilter.h"
+
+TEST_CASE("Filtering empty files list", "[io]")
+{
+
+}
+
