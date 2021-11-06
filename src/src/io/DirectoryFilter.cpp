ab091ea2382b5c13d6da6828af2f42cf6521148f
KeramKeram <rev@autograf.pl>
2021-11-06 19:44:02 +0100

    DirectoryFilter: Correct signature, add missing extension list as argument to filter function.

--- src/src/io/DirectoryFilter.cpp
+++ src/src/io/DirectoryFilter.cpp
@@ -2,8 +2,9 @@
 
 namespace io {
 
-    std::vector<std::string> DirectoryFilter::filterByExtension(std::vector<std::string> files) {
+    std::vector<std::string> DirectoryFilter::filterByExtension(std::vector<std::string> files, const std::vector<std::string> &extensions) {
         for(auto elem : files) {
+            extensions[0];
             files.clear();
         }
         return std::vector<std::string>();
--- test/DirectoryFilterTests.cpp
+++ test/DirectoryFilterTests.cpp
@@ -6,7 +6,7 @@ TEST_CASE("Filtering empty files list", "[io]")
     io::DirectoryFilter filter;
     std::vector<std::string> emptyVector;
     SECTION("Test filtering by extension empty list, return list need to be empty.") {
-        REQUIRE(filter.filterByExtension(emptyVector).size() == 0);
+        REQUIRE(filter.filterByExtension(emptyVector, {}).size() == 0);
     }
 }
 
