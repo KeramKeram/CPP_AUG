0bb26698e72e29b569865b0da6d19874dc3a4b2c
KeramKeram <rev@autograf.pl>
2021-11-06 21:08:27 +0100

    GuiController: Prepare list of files to augmentation(only jpg and png).

--- src/src/controllers/GuiController.cpp
+++ src/src/controllers/GuiController.cpp
@@ -1,8 +1,10 @@
 #include "controllers/GuiController.h"
 #include "factories/RotationFilterFactory.h"
+#include "io/Directory.h"
+#include "io/DirectoryFilter.h"
 #include "spdlog/spdlog.h"
 
-
+#include <iostream>
 namespace controllers {
 
     void GuiController::createFilters(AugumentationData data) {
@@ -19,10 +21,12 @@ namespace controllers {
     }
 
     void GuiController::okButton() {
+        auto files = io::Directory::loadFilesList(mPathImages);
+        files = io::DirectoryFilter::filterByExtension(files, {".jpg", ".png"});
         spdlog::info("call ok");
     }
 
     GuiController::GuiController() {
         mModel = std::make_unique<models::OperationModel<filters::IFilterCommand>>();
     }
-}
+}// namespace controllers
