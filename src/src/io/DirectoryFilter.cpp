e00f67e165caf691233fc1f4a4a81937df80b4a6
KeramKeram <rev@autograf.pl>
2021-11-06 20:32:42 +0100

    DirectoryFilter: Implement filter function by extension for file list.

--- src/src/io/DirectoryFilter.cpp
+++ src/src/io/DirectoryFilter.cpp
@@ -1,12 +1,22 @@
 #include "io/DirectoryFilter.h"
 
+#include <algorithm>
+#include <filesystem>
+
 namespace io {
 
     std::vector<std::string> DirectoryFilter::filterByExtension(std::vector<std::string> files, const std::vector<std::string> &extensions) {
-        for(auto elem : files) {
-            extensions[0];
-            files.clear();
+        for (const auto &extension : extensions) {
+            auto noSpacEnd = std::remove_if(files.begin(), files.end(), [extension = extension](auto file) {
+                using namespace std::filesystem;
+                path filePath(file);
+                if (auto ex = filePath.extension(); ex.empty() || ex.u8string().compare(extension) != 0) {
+                    return true;
+                }
+                return false;
+            });
+            files.erase(noSpacEnd, files.end());
         }
-        return std::vector<std::string>();
+        return files;
     }
-}
+}// namespace io
